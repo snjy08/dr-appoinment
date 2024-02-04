042d8964549cdaa0b5ebd81664d57744096eaabb
snjy08 <snjy9096@gmail.com>
2024-02-04 21:36:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//import json-server
+const jsonServer = require('json-server')
+
+//create a server application using jsonServer
+const restServer = jsonServer.create()
+
+//setup path for db.json
+const router = jsonServer.router('db.json')
+
+//return a middleware used by json server
+const middleware = jsonServer.defaults()
+
+//setup port 
+const port = 3001
+
+//use in server 
+restServer.use(middleware)
+restServer.use(router)
+
+
+//to run a port
+restServer.listen(port,()=>{
+    console.log('Rest server listening on port' +port);
+})
